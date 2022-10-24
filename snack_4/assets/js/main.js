eb73deb7e944da8706e5be6827b0499bd863acb8
diego-cavenati <diegocavenati@gmail.com>
2022-10-24 15:48:06 +0200

    jsnack 4 - add 2 array and mix into 1 in function

---
+++ snack_4/assets/js/main.js
@@ -0,0 +1,24 @@
+// Jsnack 4
+// Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall’altro
+// es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+const letter = ["a", "b", "c"];
+const number = [1, 2, 3];
+
+// let mixLetterNumber = [];
+function mixArray(firstArray, secondArray) {
+    let mixAllArray = [];
+
+    for (let i = 0; i < 3; i++) {
+        const singleFirstArray = firstArray.shift();
+        mixAllArray.push(singleFirstArray);
+        const singleSecondArray = secondArray.shift();
+        mixAllArray.push(singleSecondArray);
+    }
+
+    return mixAllArray
+
+}
+
+
+console.log(mixArray(letter, number));
